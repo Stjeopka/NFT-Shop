a6332184623bbc65f0f05eea921645be4c67ed35
MHummel <mhummel@artiso.com>
2022-03-10 15:13:54 +0100

    fix checkout layout

--- src/components/CheckOutComponent.tsx
+++ src/components/CheckOutComponent.tsx
@@ -2,7 +2,7 @@
 import * as React from 'react';
 import "../styles/CheckOut.scss"
 import {userAtom, userBalanceSelector} from "../Models/recoil-states";
-import {Button, FormControl, InputLabel, MenuItem, Select, TextField, Typography} from "@mui/material";
+import {Button, FormControl, Grid, InputLabel, MenuItem, Select, TextField, Typography} from "@mui/material";
 import {useState} from "react";
 import {useRecoilState, useRecoilValue} from "recoil";
 
@@ -15,7 +15,7 @@ enum Gender {
 export const CheckOutComponent = () => {
     const [user] = useRecoilState(userAtom);
     const [firstName, setFirstName] = useState("");
-    const [gender, setGender] = useState<Gender | undefined>();
+    const [gender, setGender] = useState<Gender | undefined>(undefined);
     const [lastName, setLastName] = useState("");
     const [address, setAddress] = useState("");
     const [email, setEmail] = useState("");
@@ -41,25 +41,43 @@ export const CheckOutComponent = () => {
                 e.preventDefault();
                 setWasBuySuccess(true);
             }}>
-                <FormControl>
-                    <InputLabel id="gender-select-label" variant={"standard"}>{genderLabelText}</InputLabel>
-                    <Select labelId="gender-select-label" id="gender-label" label={genderLabelText}
-                            onChange={(e) => setGender(Number(e.target.value))} value={gender}>
-                        <MenuItem value={Gender.male}>Mann</MenuItem>
-                        <MenuItem value={Gender.female}>Frau</MenuItem>
-                        <MenuItem value={Gender.etc}>Unbekannt</MenuItem>
-                    </Select>
-                </FormControl>
-                <TextField required type={"text"} label={"Vorname"} onChange={(e) => setFirstName(e.target.value)}
-                           value={firstName}/>
-                <TextField required type={"text"} label={"Nachname"} onChange={(e) => setLastName(e.target.value)}
-                           value={lastName}/>
-                <TextField required type={"email"} label={"email"} onChange={(e) => setEmail(e.target.value)}
-                           value={email}/>
-                <TextField required type={"text"} label={"adresse"} onChange={(e) => setAddress(e.target.value)}
-                           value={address}/>
-                <Typography>Summe: {totalBalance}</Typography>
-                <Button type={"submit"}>Bezahlen</Button>
+                <Grid container spacing={2} justifyContent={"center"} direction={"column"} alignItems={"center"}>
+                    <Grid item>
+                        <FormControl>
+                            <InputLabel id="gender-select-label" variant={"standard"}>{genderLabelText}</InputLabel>
+                            <Select labelId="gender-select-label" id="gender-label" label={genderLabelText}
+                                    onChange={(e) => setGender(Number(e.target.value))} value={gender}>
+                                <MenuItem value={Gender.male}>Mann</MenuItem>
+                                <MenuItem value={Gender.female}>Frau</MenuItem>
+                                <MenuItem value={Gender.etc}>Unbekannt</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Grid>
+                    <Grid item>
+                        <TextField required type={"text"} label={"Vorname"}
+                                   onChange={(e) => setFirstName(e.target.value)}
+                                   value={firstName}/>
+                    </Grid>
+                    <Grid item>
+                        <TextField required type={"text"} label={"Nachname"}
+                                   onChange={(e) => setLastName(e.target.value)}
+                                   value={lastName}/>
+                    </Grid>
+                    <Grid item>
+                        <TextField required type={"email"} label={"email"} onChange={(e) => setEmail(e.target.value)}
+                                   value={email}/>
+                    </Grid>
+                    <Grid item>
+                        <TextField required type={"text"} label={"adresse"} onChange={(e) => setAddress(e.target.value)}
+                                   value={address}/>
+                    </Grid>
+                    <Grid item>
+                        <Typography>Summe: {totalBalance}</Typography>
+                    </Grid>
+                    <Grid item>
+                        <Button type={"submit"}>Bezahlen</Button>
+                    </Grid>
+                </Grid>
             </form>
         </div>
     );
