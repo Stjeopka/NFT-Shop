162fdf67bb0fe74abab98c25d900d318f7be58b4
Stefzockt <64970326+Stefzockt@users.noreply.github.com>
2022-03-10 16:10:08 +0100

    About Us CSS

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -11,9 +11,9 @@ export const AboutUsComponent = (props: Props) => {
 
     return (
         <div className='top'>
+            <img className="biggerLogo" src={logoUrl} alt="NFT Trading"/>
             <h1>Building an open digital economy</h1> 
             <div className='bottom'>
-                <img className="biggerLogo" src={logoUrl} alt="NFT Trading"  />
                 <p className='paragraph'>At NFT Trading, we're excited about a brand new type of digital good called a non-fungible token, or NFT. NFTs have exciting new properties: they’re unique, provably scarce, tradeable, and usable across multiple applications. Just like physical goods, you can do whatever you want with them! You could throw them in the trash, gift them to a friend across the world, or go sell them on an open marketplace. But unlike physical goods, they're armed with all the programmability of digital goods. A core part of our vision is that open protocols like Ethereum and interoperable standards like ERC-721 and ERC-1155 will enable vibrant new economies. We're building tools that allow consumers to trade their items freely, creators to launch new digital works, and developers to build rich, integrated marketplaces for their digital items.</p>
             </div>
         </div>
